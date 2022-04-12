a8189668489938c523d6764ec33e8ed875618091
Hunseong <phunseong@gmail.com>
2022-04-12 16:48:07 +0900

    initial commit

---
+++ src/main/java/hellojpa/jpashopp/JpashoppApplication.java
@@ -0,0 +1,13 @@
+package hellojpa.jpashopp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpashoppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpashoppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hellojpa/jpashopp/JpashoppApplicationTests.java
@@ -0,0 +1,13 @@
+package hellojpa.jpashopp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpashoppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
