f54c6d231e7be58e18e5dfa62dec1ec48abd6ba4
Hunseong <phunseong@gmail.com>
2022-04-15 12:35:08 +0900

    Order V1 주문조회 - Hibernate5Module을 통해 초기화 되지 않은 Proxy 객체 null 처리

--- src/main/java/hellojpa/jpashopp/JpashoppApplication.java
+++ src/main/java/hellojpa/jpashopp/JpashoppApplication.java
@@ -1,13 +1,20 @@
 package hellojpa.jpashopp;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpashoppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JpashoppApplication.class, args);
+    }
+
+    @Bean
+    Hibernate5Module hibernate5Module() {
+        return new Hibernate5Module();
     }
 
 }
