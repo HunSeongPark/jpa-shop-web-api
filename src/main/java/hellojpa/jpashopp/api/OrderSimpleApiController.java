c3a64a54178e27c03f96fa51a20f9fbd9ed4a425
Hunseong <phunseong@gmail.com>
2022-04-15 12:52:22 +0900

    Order V3 주문조회 - fetch join을 통한 최적화

--- src/main/java/hellojpa/jpashopp/api/OrderSimpleApiController.java
+++ src/main/java/hellojpa/jpashopp/api/OrderSimpleApiController.java
@@ -36,11 +36,30 @@ public class OrderSimpleApiController {
         return orders;
     }
 
-    // 쿼리 횟수 : Order 조회 1 + 각 Order 별 Member LAZY 조회 N + 각 Order 별 Delivery LAZY 조회 N
-    // 1+N+N
+    /**
+     * V2
+     * 쿼리 횟수 : Order 조회 1 + 각 Order 별 Member LAZY 조회 N + 각 Order 별 Delivery LAZY 조회 N
+     * 영속성 컨텍스트 캐싱 없는 경우(최악의 경우) Query 1+N+N
+     */
     @GetMapping("/api/v2/simple-orders")
     public Result<List<SimpleOrderDto>> ordersV2() {
         List<Order> orders = orderRepository.findAll(new OrderSearch());
+
+        List<SimpleOrderDto> collect = orders.stream()
+                .map(SimpleOrderDto::new)
+                .collect(Collectors.toList());
+
+        return new Result<>(collect);
+    }
+
+    /**
+     * V3
+     * 쿼리 횟수 : fetch join을 통해 Order o + Member o.member + Delivery o.delivery를 쿼리 한번으로 조회
+     * Query 1
+     */
+    @GetMapping("/api/v3/simple-orders")
+    public Result<List<SimpleOrderDto>> ordersV3() {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery();
 
         List<SimpleOrderDto> collect = orders.stream()
                 .map(SimpleOrderDto::new)
