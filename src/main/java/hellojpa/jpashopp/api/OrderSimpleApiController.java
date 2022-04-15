d142f42b1da0fb975cb115d246957d1bd96bd23e
Hunseong <phunseong@gmail.com>
2022-04-15 12:46:41 +0900

    Order V2 주문조회 - 엔티티를 DTO로 변환

--- src/main/java/hellojpa/jpashopp/api/OrderSimpleApiController.java
+++ src/main/java/hellojpa/jpashopp/api/OrderSimpleApiController.java
@@ -1,14 +1,21 @@
 package hellojpa.jpashopp.api;
 
+import hellojpa.jpashopp.domain.Address;
 import hellojpa.jpashopp.domain.Order;
 import hellojpa.jpashopp.domain.OrderSearch;
+import hellojpa.jpashopp.domain.OrderStatus;
 import hellojpa.jpashopp.repository.OrderRepository;
 import hellojpa.jpashopp.service.OrderService;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.aspectj.weaver.ast.Or;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by Hunseong on 2022/04/15
@@ -27,5 +34,47 @@ public class OrderSimpleApiController {
             order.getDelivery().getStatus(); // 내부 property getter를 통해 LAZY 강제 초기화
         }
         return orders;
+    }
+
+    // 쿼리 횟수 : Order 조회 1 + 각 Order 별 Member LAZY 조회 N + 각 Order 별 Delivery LAZY 조회 N
+    // 1+N+N
+    @GetMapping("/api/v2/simple-orders")
+    public Result<List<SimpleOrderDto>> ordersV2() {
+        List<Order> orders = orderRepository.findAll(new OrderSearch());
+
+        List<SimpleOrderDto> collect = orders.stream()
+                .map(SimpleOrderDto::new)
+                .collect(Collectors.toList());
+
+        return new Result<>(collect);
+    }
+
+
+    // ================ DTO ================= //
+
+    @Getter
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    static class SimpleOrderDto {
+
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        // from Entity
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName(); // LAZY 초기화
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress(); // LAZY 초기화
+        }
     }
 }
